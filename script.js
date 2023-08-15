2128d4ce0e99cdba625ab2fea103dc7707f33fd2
Henrique Arsenio <105521096+henriquearsenio@users.noreply.github.com>
2023-08-15 11:17:48 -0300

    finalização js

---
+++ script.js
@@ -0,0 +1,13 @@
+const formulario = document.getElementById("formulario-cadastro");
+
+formulario.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    console.log("Nome: ${nome}");
+    console.log("Email: ${email}");
+    console.log("Senha: ${senha}");
+})
